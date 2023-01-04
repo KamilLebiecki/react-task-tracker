fa3ede91ef85636cb426dc2cf63d19a09a2fe1ee
Kamil Lebiecki <2973470+kamillebiecki@users.noreply.github.com>
2023-01-04 11:06:43 +0100

    adding a task component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 const App = () => {
+	const [tasks, setTasks] = useState([
+		{ id: 1, text: 'Walk the dog', day: 'Jan 5th at 11:00am', reminder: true },
+		{ id: 2, text: 'Lunch', day: 'Jan 5th at 12:00pm', reminder: false },
+		{ id: 3, text: 'Make laundry', day: 'Jan 5th at 12:30pm', reminder: true },
+		{ id: 5, text: 'Workout', day: 'Jan 5th at 08:00am', reminder: false },
+		{ id: 6, text: 'Jogging', day: 'Jan 5th at 5:00pm', reminder: false },
+	]);
+
 	return (
 		<div className="container">
 			<Header />
-			<Tasks />
+			<Tasks tasks={tasks} />
 		</div>
 	);
 };
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,14 +1,4 @@
-import { useState } from 'react';
-
-// const tasks = [
-// 	{ id: 1, text: 'Walk the dog', day: 'Jan 5th at 11:00am', reminder: true },
-// 	{ id: 2, text: 'Lunch', day: 'Jan 5th at 12:00pm', reminder: false },
-// 	{ id: 3, text: 'Make laundry', day: 'Jan 5th at 12:30pm', reminder: true },
-// 	{ id: 5, text: 'Workout', day: 'Jan 5th at 08:00am', reminder: false },
-// 	{ id: 6, text: 'Jogging', day: 'Jan 5th at 5:00pm', reminder: false },
-// ];
-
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
 	return (
 		<>
 			{tasks.map((task) => (
